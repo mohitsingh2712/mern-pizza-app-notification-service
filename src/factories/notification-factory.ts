b631565bfb208345a4befa927f54122cfede5cc7
mo14337 <mohitsingh14336@gmail.com>
2025-04-30 21:25:36 +0530

    made createNotificaionTransport function singleton

--- src/factories/notification-factory.ts
+++ src/factories/notification-factory.ts
@@ -1,9 +1,20 @@
 import { MailTransport } from "../mail";
+import { NotificationTransport } from "../types/notification-types";
 
+const transports: NotificationTransport[] = [];
 export const createNotificaionTransport = (type: "mail" | "sms") => {
   switch (type) {
-    case "mail":
-      return new MailTransport();
+    case "mail": {
+      const requiredTransport = transports.find(
+        (transport) => transport instanceof MailTransport,
+      );
+      if (requiredTransport) {
+        return requiredTransport;
+      }
+      const instance = new MailTransport();
+      transports.push(instance);
+      return instance;
+    }
     case "sms":
       throw new Error("SMS transport not implemented");
     default:
